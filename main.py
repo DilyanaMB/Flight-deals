b5f091143109739dcc1fe7e44a9739d9ced72d3b
dilyanabodurova <diliana.bodurova@abv.bg>
2026-01-14 08:57:16 +0200

    in progress

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+import requests
+
+API_KEY='FyG4Srx47QoH3KTmCqhEb7GkMpM1FKMd'
+API_SECRET='dT10vDhU4EKqvyzC'
+password='5k*vbnmF72C?Bn8'
+email='dilyana.m.bodurova@gmail.com'
+auth_url='https://test.api.amadeus.com/v1/security/oauth2/token'
+headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+body={f'grant_type=client_credentials&client_id={API_KEY}&client_secret={API_SECRET}'}
+response = requests.post(url=auth_url,headers=headers, data=body)
+print(response.json())
+
